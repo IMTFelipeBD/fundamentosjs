7b2555eb772f1597bfd1bbd20324e6a79b2a95c6
Pips <jfvb_2192@hotmail.com>
2019-05-04 20:38:40 -0500

    Subi todos los archivos

---
+++ clase1.js
@@ -0,0 +1,7 @@
+var nombre = 'Felipe', apellido
+var apellido;
+
+nombre = prompt("Ingresa tu nombre")
+apellido = 'Vazquez '
+
+todo = "Hola  " + nombre + apellido 
---
+++ clase4.js
@@ -0,0 +1,9 @@
+var nombre = 'Felipe', edad = 28 
+
+function imprimirEdad(n,e){
+    console.log(`${n} tiene ${e} años`)
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Vicky', 28)
+imprimirEdad('Dora', 29)
