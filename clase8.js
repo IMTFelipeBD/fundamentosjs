d911731ca3ed03a50abd2fd9b56d78d216e4df98
Pips <jfvb_2192@hotmail.com>
2019-05-04 22:44:57 -0500

    ejercicios 8-9

---
+++ clase8.js
@@ -0,0 +1,40 @@
+var felipe = {
+    nombre: 'Felipe',
+    apellido: 'Vazquez',
+    edad: 28
+}
+var yolo = {
+    nombre: 'Yolo',
+    apellido: 'Jimenez',
+    edad: 27
+}
+
+function imprimirNombreEnMayusculas({ nombre }) {
+    // var nombre = persona.nombre.toUpperCase()
+    var { nombre } = persona.toUpperCase()
+    console.log(nombre.toUpperCase())
+}
+
+//imprimirNombreEnMayusculas(felipe)
+//imprimirNombreEnMayusculas(yolo)
+//imprimirNombreEnMayusculas({ apellido: 'Pepito' })
+
+function imprimirNombreYEdad({ nombre, edad }) {
+    // funcion para imprimir nombre y edad
+    //Hola me llamo Felipe y tengo 28 años
+    //Hola me llamo Yolo y tengo 27 años
+    console.log(`Hola me llamo ${nombre} y tengo ${edad} años`)
+
+}
+
+imprimirNombreYEdad(felipe)
+imprimirNombreYEdad(yolo)
+
+
+function cumpleanos(persona){
+    return {
+        ...persona,
+        edad: persona.edad + 1
+    }
+}
+
---
+++ clase9.js
@@ -0,0 +1,9 @@
+var x = 4, y = '4'
+
+var felipe = {
+    nombre:'Felipe'
+}
+
+var yolo = {
+    nombre:'Yolo'
+}
