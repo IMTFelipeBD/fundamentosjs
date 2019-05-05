3e48cf02e3f6941d381f7e5a305e03b96612d60c
Pips <jfvb_2192@hotmail.com>
2019-05-04 22:00:21 -0500

    Ejercicios 5 al 7

---
+++ clase6.js
@@ -0,0 +1,19 @@
+var felipe = {
+    nombre: 'Felipe',
+    apellido: 'Vazquez',
+    edad: 28 
+}
+var yolo = {
+    nombre: 'Yolo',
+    apellido: 'Jimenez',
+    edad: 28 
+}
+
+function imprimirNombreEnMayusculas({nombre}) {
+   /* var nombre = persona.nombre.toUpperCase()*/
+    console.log(nombre.toUpperCase())
+}
+
+imprimirNombreEnMayusculas(felipe)
+imprimirNombreEnMayusculas(yolo)
+imprimirNombreEnMayusculas({apellido: 'Pepito'})
